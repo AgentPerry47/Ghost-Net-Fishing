ef5b67bb793b1f7d9f617334519566098432a3c2
AgentPerry47 <pbecker-sls@web.de>
2025-11-24 11:24:27 +0100

    Initial commit

---
+++ demo/src/main/java/com/example/demo/repository/GhostNetRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.GhostNet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface GhostNetRepository extends JpaRepository<GhostNet, Long> {}
+
